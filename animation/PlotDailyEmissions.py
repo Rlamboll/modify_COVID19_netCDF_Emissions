02b79603d0a8f5f4545d3d2d7b340ee6a038a19a
Lamboll <rlamboll@ic.ac.uk>
2020-07-29 18:41:16 +0100

    Updated to include grey outline of continents

--- animation/PlotDailyEmissions.py
+++ animation/PlotDailyEmissions.py
@@ -1,8 +1,7 @@
+import cartopy.crs as ccrs
 import datetime
-import math
-import matplotlib.colors
+import matplotlib
 import netCDF4 as nc
-import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.animation as anim
 import seaborn
@@ -12,83 +11,86 @@ import seaborn
 baseline_file = "../output/aerosols/daily/cut_SO2-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_baseline.nc"
 covid_file = "../output/aerosols/daily/cut_SO2-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_1_year.nc"
 varname = "SO2_em_anthro"
+title_str = "Reduction in SO$_2$ emissions resulting from COVID-19 \n Date: {} {}."
 var_label = "SO$_2$ emissions ({})"
-units = "kg/km$^2$/s"
+units = "kg/km$^2$/day"
 covid_data = nc.Dataset(covid_file)
 baseline_data = nc.Dataset(baseline_file)
 covid = covid_data.variables[varname][:]
 base = baseline_data.variables[varname][:]
-
+lats = covid_data.variables['lat'][:]
+lons = covid_data.variables['lon'][:]
+img_extent = [min(lons), max(lons), min(lats), max(lats)]
 sect_ax = 1
-basesum = (base.sum(axis=sect_ax) - covid.sum(axis=sect_ax)) * 10 ** 6
+basesum = (base.sum(axis=sect_ax) - covid.sum(axis=sect_ax)) * 10 ** 6 * 3600 * 24
 base_time = baseline_data.variables["time"][:]
 startind = 0
 endind = 180
-lowlim = 6
-savename = "animated_COVID_{}_v2.gif".format(varname)
+lowlim = 5.5
+savename = "animated_COVID_dif_{}_v3.gif".format(varname)
 
 # We want to plot some transform of the data to prevent nans from log(0)
 max_data = basesum.max()
+
 def data_transform(data, max_data):
     return data + max_data * 1e-15
+
 
 def date_conv(days):
     date = datetime.datetime(year=2015, month=1, day=2) + datetime.timedelta(days=days)
     return date.strftime("%b"), date.day
 
+
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure(figsize=(8, 4))
-cbar_ticks = [
-    math.pow(10, i) for i in range(
-        math.floor(np.log10(max_data) - lowlim), math.ceil(np.log10(max_data))
-    )
-]
+ax = plt.axes(projection=ccrs.PlateCarree())
 
+cmap = matplotlib.cm.get_cmap("YlGnBu")
+cmap.set_bad('white', 1.)
 plot_options = {
-    "xticklabels": False, "yticklabels": False,
     "vmax": max_data,
     "vmin": max_data / 10**lowlim,
-    "norm": matplotlib.colors.LogNorm(vmin=max_data / 10**lowlim, vmax=max_data)
+    "norm": matplotlib.colors.LogNorm(vmin=max_data / 10**lowlim, vmax=max_data),
+    "cmap": cmap,
+    "origin": "upper",
+    "extent": img_extent,
+    "transform": ccrs.PlateCarree(),
 }
-line = seaborn.heatmap(
-    data_transform(basesum[startind, ::-1, :], max_data),
-    cbar_kws={"ticks": cbar_ticks, 'label': var_label.format(units)},
-    **plot_options
-)
+line = ax.imshow(data_transform(basesum[startind, ::-1, :], max_data), **plot_options)
+ax.coastlines(color="lightgrey")
+plt.title(title_str)
 month, day = date_conv(base_time[startind])
-plt.title("Date: {} {}".format(month, day))
+plt.title(title_str.format(month, day))
+cb = fig.colorbar(line, ax=ax, format="%d")
+cb.ax.set_ylabel("Reduction in " + var_label.format(units))
+cb.ax.invert_yaxis()
+plt.text(185, 60, "White = increased activity", rotation=90)
 
 # initialization function: plot the background of each frame
 def init():
-    line = seaborn.heatmap(
-        data_transform(basesum[startind, ::-1, :], max_data),
-        cbar=False,
-        **plot_options
-    )
+    line = ax.imshow(data_transform(basesum[startind, ::-1, :], max_data),
+                     **plot_options)
     month, day = date_conv(base_time[startind])
-    plt.title("Date: {} {}".format(month, day))
+    plt.title(title_str.format(month, day))
     return line,
 
 # animation function.  This is called sequentially
 def animate(i):
-    line = seaborn.heatmap(
-        data_transform(basesum[startind + i, ::-1, :], max_data),
-        cbar=False,
-        **plot_options
-    )
+    line = ax.imshow(data_transform(basesum[startind + i, ::-1, :], max_data),
+                     **plot_options)
     month, day = date_conv(base_time[startind + i])
-    plt.title("Date: {} {}".format(month, day))
+    plt.title(title_str.format(month, day))
     return line,
 
 
 writer = anim.writers['pillow']
-writer = writer(fps=3, metadata=dict(artist='Robin Lamboll'), bitrate=-1)
+writer = writer(fps=4, metadata=dict(artist='Robin Lamboll'), bitrate=-1)
 
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 animation_inst = anim.FuncAnimation(
     fig, animate, init_func=init, frames=endind - startind,
-    interval=20, blit=False
+    interval=20, blit=True
 )
 # Save the animation
 animation_inst.save(savename, writer=writer)
