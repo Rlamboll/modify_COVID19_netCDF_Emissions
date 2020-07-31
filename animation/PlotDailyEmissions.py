a2caa1fa05ad0c2ad880d1459931c9fd2de445f9
Lamboll <rlamboll@ic.ac.uk>
2020-07-31 12:35:31 +0100

    Working version of NOx calc including aviation

--- animation/PlotDailyEmissions.py
+++ animation/PlotDailyEmissions.py
@@ -2,31 +2,34 @@ import cartopy.crs as ccrs
 import datetime
 import matplotlib
 import netCDF4 as nc
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 from matplotlib import pyplot as plt
 import matplotlib.animation as anim
 
 
 # Load data
-include_aviation = False
+include_aviation = True
 if not include_aviation:
     baseline_file = "../output/aerosols/daily/cut_SO2-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_baseline.nc"
     covid_file = "../output/aerosols/daily/cut_SO2-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_1_year.nc"
     varname = "SO2_em_anthro"
+    title_str = "Fraction of usual SO$_2$ emissions due to COVID-19 \n Date: {} {}."
 else:
-    baseline_file = "../output/aerosols/daily/cut_NOx-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_1_year.nc"
-    covid_file = "../output/aerosols/daily/cut_NOx-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_baseline.nc"
+    baseline_file = "../output/aerosols/daily/cut_NOx-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_baseline.nc"
+    covid_file = "../output/aerosols/daily/cut_NOx-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.ncdaily_v4.nc_1_year.nc"
     av_varname = "NOx_em_AIR_anthro"
-    aviation_base_file = "../output/aviation/cut_CO2-em-AIR-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.nc_baseline_v4.5.nc"
-    aviation_covid_file = "../output/aviation/cut_CO2-em-AIR-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.nc_flightrd_mp06_v4.5.nc"
+    aviation_base_file = "../output/aviation/cut_NOx-em-AIR-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.nc_baseline_v4.5.nc"
+    aviation_covid_file = "../output/aviation/cut_NOx-em-AIR-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-MESSAGE-GLOBIOM-ssp245-1-1_gn_201501-210012.nc_v4.5.nc_flightrd_mp06_daily_v4.5.nc"
     height_ax = 1
     base_av_ds = nc.Dataset(aviation_base_file)
     covid_av_ds = nc.Dataset(aviation_covid_file)
     covid_av = covid_av_ds.variables[av_varname][:]
     base_av = base_av_ds.variables[av_varname][:]
-    assert covid_av_ds.variables["time"][:] == base_av_ds.variables["time"][:]
+    assert np.allclose(covid_av_ds.variables["time"][:], base_av_ds.variables["time"][:])
     varname = "NOx_em_anthro"
-title_str = "Proportional reduction in SO$_2$ emissions resulting from COVID-19 \n Date: {} {}."
-var_label = "SO$_2$ emissions reduction"
+    title_str = "Fraction of usual NO$_x$ emissions due to COVID-19 \n Date: {} {}."
+
 covid_data = nc.Dataset(covid_file)
 baseline_data = nc.Dataset(baseline_file)
 covid = covid_data.variables[varname][:]
@@ -35,20 +38,7 @@ lats = covid_data.variables['lat'][:]
 lons = covid_data.variables['lon'][:]
 img_extent = [min(lons), max(lons), min(lats), max(lats)]
 sect_ax = 1
-if not include_aviation:
-    basesum = (covid.sum(axis=sect_ax) / base.sum(axis=sect_ax))
-else:
-    basesum = (covid.sum(axis=sect_ax) + covid_av.sum(axis=height_ax)) / \
-              (base.sum(axis=sect_ax) + base_av.sum(axis=height_ax))
-base_time = baseline_data.variables["time"][:]
-startind = 0
-endind = 180
-lowlim = 5.5
-savename = "output/animated_COVID_rel_lin_{}_v4.gif".format(varname)
 
-# We want to plot some transform of the data to prevent nans from log(0)
-max_data = basesum.max()
-min_data = basesum.min()
 
 def data_transform(data, max_data):
     return data.filled(1)
@@ -58,6 +48,38 @@ def date_conv(days):
     date = datetime.datetime(year=2015, month=1, day=2) + datetime.timedelta(days=days)
     return date.strftime("%b"), date.day
 
+
+def interpolate_missing_times(ds, have_times, want_times, lats, lons):
+    interpolating_fn = RegularGridInterpolator((have_times, lats, lons), ds)
+    return interpolating_fn([(x, y, z) for x in want_times.filled(np.nan) for y in lats.filled(np.nan) for z in lons.filled(np.nan)])
+
+
+base_time = baseline_data.variables["time"][:]
+startind = 0
+endind = 180
+lowlim = 5.5
+savename = "output/animated_COVID_rel_lin_{}_v5.gif".format(varname)
+if not include_aviation:
+    basesum = (covid.sum(axis=sect_ax) / base.sum(axis=sect_ax))
+else:
+    base_av_int = interpolate_missing_times(
+        base_av.sum(axis=height_ax),
+        base_av_ds.variables["time"][:],
+        baseline_data.variables["time"][startind:endind],
+        lats, lons
+    ).reshape(endind - startind, len(lats), len(lons))
+    covid_av_int = interpolate_missing_times(
+        covid_av.sum(axis=height_ax),
+        covid_av_ds.variables["time"][:],
+        baseline_data.variables["time"][startind:endind],
+        lats, lons
+    ).reshape(endind - startind, len(lats), len(lons))
+    basesum = (covid.sum(axis=sect_ax)[startind:endind, ...] + covid_av_int) / \
+              (base.sum(axis=sect_ax)[startind:endind, ...] + base_av_int)
+
+# We want to plot some transform of the data to prevent nans from log(0)
+max_data = basesum.max()
+min_data = basesum.min()
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure(figsize=(8, 4))
@@ -79,7 +101,7 @@ plt.title(title_str)
 month, day = date_conv(base_time[startind])
 plt.title(title_str.format(month, day))
 cb = fig.colorbar(line, ax=ax, format="%g")
-cb.ax.set_ylabel("Proportional reduction")
+cb.ax.set_ylabel("Emissions proportion")
 
 # initialization function: plot the background of each frame
 def init():
